bbb942507476ab6b11929a684569cd63afb0bcbd
xac <xac@pop-os.localdomain>
2022-01-27 11:17:41 +0100

    Fixed sanity query

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -1,51 +1,70 @@
-import React, {useState, useRef, useEffect} from 'react';
-import {HiMenu} from 'react-icons/hi';
-import {AiFillCloseCircle} from 'react-icons/ai';
-import {Link, Route, Routes} from 'react-router-dom';
-import {SideBar, UserProfile} from '../components';
-import {client} from '../client';
-import logo from '../assets/logo.png'
-import Pins from './Pins';
-import {userQuery} from '../utils/data'
+import React, {useState, useRef, useEffect} from "react";
+import {HiMenu} from "react-icons/hi";
+import {AiFillCloseCircle} from "react-icons/ai";
+import {Link, Route, Routes} from "react-router-dom";
+import {SideBar, UserProfile} from "../components";
+import {client} from "../client";
+import logo from "../assets/logo.png";
+import Pins from "./Pins";
+import {userQuery} from "../utils/data";
 
 function Home(props) {
-    const [toggleSidebar, setToggleSidebar] = useState(false);
-    const [user, setUser] = useState(null);
-    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
-    
-    useEffect(() => {
-        const query = userQuery(userInfo?.googleId);
-        
-        client.fetch(query)
-        .then((data) => {
-            setUser(data[0]);
-            console.log(query)
-        })
-    },[]);
-    return (
-        <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
-            <div className='hidden md:flex h-screen flex-initial'>
-                <SideBar user={user && user} closeToggle={setToggleSidebar} />
-            </div>
-            <div className='flex md:hidden flex-row'>
-                <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)}/>
-                <Link to='/'>
-                    <img src={logo} alt='logo' className='w-28'/>
-                </Link>
-                 <Link to={`user-profile/${user?._id}`}> 
-                     <img src={user?.image} alt='user-pic' className='w-28'/> 
-                 </Link>  
-            </div>
-            {toggleSidebar && (
-                <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
-                    <div className='absolute w-full flex justify-end items-center p-2'>
-                        <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />   
-                    </div>
-                    <SideBar user={user && user} closeToggle={setToggleSidebar} />
-                </div>
-            )}
-        </div>
-    );
+	const [toggleSidebar, setToggleSidebar] = useState(false);
+	const [user, setUser] = useState(null);
+
+	const userInfo =
+		localStorage.getItem("user") !== "undefined"
+			? JSON.parse(localStorage.getItem("user"))
+			: localStorage.clear();
+
+	useEffect(() => {
+		const query = userQuery(userInfo?.googleId);
+
+		console.log("Client:", client);
+		client.fetch(query).then((arg) => {
+			setUser(arg[0]);
+
+			console.log("Query:", query);
+			console.log("userInfo:", userInfo);
+			console.log("Data:", arg);
+		});
+	}, []);
+
+	return (
+		<div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+			<div className="hidden md:flex h-screen flex-initial">
+				<SideBar user={user && user} closeToggle={setToggleSidebar} />
+			</div>
+			<div className="flex md:hidden flex-row">
+				<HiMenu
+					fontSize={40}
+					className="cursor-pointer"
+					onClick={() => setToggleSidebar(true)}
+				/>
+				<Link to="/">
+					<img src={logo} alt="logo" className="w-28" />
+				</Link>
+				<Link to={`user-profile/${user?._id}`}>
+					<img src={user?.image} alt="user-pic" className="w-28" />
+				</Link>
+			</div>
+			{toggleSidebar && (
+				<div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+					<div className="absolute w-full flex justify-end items-center p-2">
+						<AiFillCloseCircle
+							fontSize={30}
+							className="cursor-pointer"
+							onClick={() => setToggleSidebar(false)}
+						/>
+					</div>
+					<SideBar
+						user={user && user}
+						closeToggle={setToggleSidebar}
+					/>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Home;
