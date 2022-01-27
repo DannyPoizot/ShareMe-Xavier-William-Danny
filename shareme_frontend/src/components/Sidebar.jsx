d3ba1fb4e76f1eb70ea6a7e57feaae1190b69ae8
xac <xac@pop-os.localdomain>
2022-01-27 14:35:45 +0100

    Added categories to sidebar

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -12,6 +12,15 @@ const isNotActiveStyle =
 const isActiveStyle =
 	"flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
+const categories = [
+	{name: "Animals"},
+	{name: "Wallpapers"},
+	{name: "Photography"},
+	{name: "Gaming"},
+	{name: "Coding"},
+	{name: "Other"},
+];
+
 function Sidebar({user, closeToggle}) {
 	const handleCloseSidebar = () => {
 		if (closeToggle) closeToggle(false);
@@ -34,6 +43,7 @@ function Sidebar({user, closeToggle}) {
 						className={({isActive}) =>
 							isActive ? isActiveStyle : isNotActiveStyle
 						}
+						onClick={handleCloseSidebar}
 					>
 						<RiHomeFill />
 						Home
@@ -42,8 +52,36 @@ function Sidebar({user, closeToggle}) {
 					<h3 className="mt-2 px-5 text-base 2xl:text-xl ">
 						Discover categories
 					</h3>
+					{categories
+						.slice(0, categories.length - 1)
+						.map((category) => (
+							<NavLink
+								to={`/category/${category.name}`}
+								className={({isActive}) =>
+									isActive ? isActiveStyle : isNotActiveStyle
+								}
+								onClick={handleCloseSidebar}
+								key={category.name}
+							>
+								{category.name}
+							</NavLink>
+						))}
 				</div>
 			</div>
+
+			{user && (
+				<Link
+					to={`user-profile/${user._id}`}
+					className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+				>
+					<img
+						src={user.image}
+						className="w-10 h-10 rounded-full"
+						alt="user-profile"
+					/>
+					<p>{user.userName}</p>
+				</Link>
+			)}
 		</div>
 	);
 }
