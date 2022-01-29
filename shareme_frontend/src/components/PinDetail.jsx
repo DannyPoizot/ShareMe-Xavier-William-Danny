37fdb50b47039335e4c91e3ce5f80122b34b2e81
xac <xac@pop-os.localdomain>
2022-01-29 22:29:40 +0100

    Added download, post url, about, posted by to pinDetail; working on comments

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -18,8 +18,6 @@ const PinDetail = ({user}) => {
 	// Fetch dynamic parameters (see Route path)
 	const {pinId} = useParams();
 
-	if (!pinDetail) return <Spinner message="Loading pin..." />;
-
 	const fetchPinDetails = () => {
 		let query = pinDetailQuery(pinId);
 
@@ -38,7 +36,70 @@ const PinDetail = ({user}) => {
 		}
 	};
 
-	return <div>PinDetail</div>;
+	useEffect(() => {
+		fetchPinDetails();
+	}, [pinId]);
+
+	if (!pinDetail) return <Spinner message="Loading pin..." />;
+
+	return (
+		<div
+			className="flex xl-flex-row flex-col m-auto bg-white"
+			style={{maxWidth: "1500px", borderRadius: "32px"}}
+		>
+			<div className="flex justify-center items-center md:items-start flex-initial">
+				<img
+					src={pinDetail?.image && urlFor(pinDetail.image).url()}
+					className="rounded-t-3xl rounded-b-lg"
+					alt="user-post"
+				/>
+			</div>
+			<div className="w-full p-5 flex-1 xl:min-w-620">
+				<div className="flex items-center justify-between">
+					<div className="flex gap-2 items-center">
+						<a
+							href={`${pinDetail.image?.asset?.url}?dl=`}
+							download
+							onClick={(e) => e.stopPropagation()}
+							className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+						>
+							<MdDownloadForOffline />
+						</a>
+					</div>
+					<a
+						href={pinDetail.destination}
+						target="_blank"
+						rel="noreferrer"
+					>
+						{pinDetail.destination.length > 20
+							? pinDetail.destination.slice(8, 20)
+							: pinDetail.destination.slice(8)}
+						{/* {pinDetail.destination} */}
+					</a>
+				</div>
+				<div>
+					<h1 className="text-4xl font-bold break-words mt-3">
+						{pinDetail.title}
+					</h1>
+					<p className="mt-3">{pinDetail.about}</p>
+				</div>
+				<Link
+					to={`user-profile/${user?._id}`}
+					className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+				>
+					<img
+						className="w-8 h-8 rounded-full object-cover"
+						src={pinDetail.postedBy?.image}
+						alt="user-profile"
+					/>
+					<p className="font-semibold capitalize">
+						{pinDetail.postedBy?.userName}
+					</p>
+				</Link>
+				<h2>Comments</h2>
+			</div>
+		</div>
+	);
 };
 
 export default PinDetail;
