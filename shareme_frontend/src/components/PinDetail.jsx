961c02eb6111bebd890436f03eecb95b528ee195
xac <xac@pop-os.localdomain>
2022-01-29 23:54:14 +0100

    Implemented commenting ability

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -12,11 +12,37 @@ import Spinner from "./Spinner";
 const PinDetail = ({user}) => {
 	const [pins, setPins] = useState(null);
 	const [pinDetail, setPinDetail] = useState(null);
-	const [comment, setComment] = useState("null");
-	const [addingComming, setAddingComment] = useState(false);
+	const [comment, setComment] = useState("");
+	const [addingComment, setAddingComment] = useState(false);
 	// ID of current post we're on
 	// Fetch dynamic parameters (see Route path)
 	const {pinId} = useParams();
+
+	const addComment = () => {
+		if (comment) {
+			setAddingComment(true);
+
+			client
+				.patch(pinId)
+				.setIfMissing({comments: []})
+				.insert("after", "comments[-1]", [
+					{
+						comment,
+						_key: uuidv4(),
+						postedBy: {
+							_type: "postedBy",
+							_ref: user._id,
+						},
+					},
+				])
+				.commit()
+				.then(() => {
+					fetchPinDetails();
+					setComment("");
+					setAddingComment(false);
+				});
+		}
+	};
 
 	const fetchPinDetails = () => {
 		let query = pinDetailQuery(pinId);
@@ -84,7 +110,7 @@ const PinDetail = ({user}) => {
 					<p className="mt-3">{pinDetail.about}</p>
 				</div>
 				<Link
-					to={`user-profile/${user?._id}`}
+					to={`user-profile/${pinDetail?.postedBy._id}`}
 					className="flex gap-2 mt-5 items-center bg-white rounded-lg"
 				>
 					<img
@@ -116,6 +142,30 @@ const PinDetail = ({user}) => {
 							</div>
 						</div>
 					))}
+				</div>
+
+				<div className="flex flex-wrap mt-6 gap-3">
+					<Link to={`user-profile/${user?._id}`}>
+						<img
+							className="w-10 h-10 rounded-full cursor-pointer"
+							src={user.image}
+							alt="user-profile"
+						/>
+					</Link>
+					<input
+						className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+						type="text"
+						placeholder="Add a comment"
+						value={comment}
+						onChange={(e) => setComment(e.target.value)}
+					/>
+					<button
+						type="button"
+						className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+						onClick={addComment}
+					>
+						{addingComment ? "Posting comment..." : "Post"}
+					</button>
 				</div>
 			</div>
 		</div>
