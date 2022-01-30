e49bfbbc1872b133a99db29a5cc9d299c9dabbaf
xac <xac@pop-os.localdomain>
2022-01-30 12:04:08 +0100

    Added random banner, user pic, logout button+function

--- shareme_frontend/src/components/UserProfile.jsx
+++ shareme_frontend/src/components/UserProfile.jsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
+import {AiOutlineLogout} from "react-icons/ai";
+import {useNavigate, useParams} from "react-router-dom";
+import {GoogleLogout} from "react-google-login";
+
+import {
+	userCreatedPinsQuery,
+	userQuery,
+	userSavedPinsQuery,
+} from "../utils/data";
+import {client} from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+// url that returns a random image
+const randomImage =
+	"https://source.unsplash.com/1600x900/?nature,photography,technology";
 
 function UserProfile(props) {
-    return (
-        <div>
-            UserProfile
-        </div>
-    );
+	const [user, setUser] = useState(null);
+	const [pins, setPins] = useState(null);
+	const [text, setText] = useState("Created"); // Created | Saved
+	const [activeBtn, setActiveBtn] = useState("created");
+	const navigate = useNavigate();
+	const {userId} = useParams();
+
+	const User =
+		localStorage.getItem("user") !== "undefined"
+			? JSON.parse(localStorage.getItem("user"))
+			: localStorage.clear();
+
+	useEffect(() => {
+		const query = userQuery(userId);
+
+		client.fetch(query).then((data) => {
+			setUser(data[0]);
+		});
+	}, [userId]);
+
+	if (!user) {
+		return <Spinner message="Loading profile..." />;
+	}
+
+	const logout = () => {
+		localStorage.clear();
+		navigate("/login");
+	};
+	return (
+		<div className="relative pb-2 h-full justify-center items-center">
+			<div className="flex flex-col pb-5">
+				<div className="relative flex flex-col mb-7">
+					<div className="flex flex-col justify-center items-center">
+						<img
+							src={randomImage}
+							className="w-full h-370 2xl:h-510 shadow-lg object-cover"
+							alt="banner-pic"
+						/>
+						<img
+							className="rounded-full w-20 h-20 -mt-10 shadow-lg object-cover"
+							src={user?.image}
+							alt="user-pic"
+						/>
+						<h1 className="font-bold text-3xl text-center mt-3">
+							{user?.userName}
+						</h1>
+						<div className="absolute top-0 z-1 right-0 p-2">
+							{userId === User.googleId && (
+								<GoogleLogout
+									clientId={
+										process.env.REACT_APP_GOOGLE_API_TOKEN
+									}
+									render={(renderProps) => (
+										<button
+											type="button"
+											className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+											onClick={renderProps.onClick}
+											disabled={renderProps.disabled}
+										>
+											<AiOutlineLogout
+												color="red"
+												fontSize={21}
+											/>
+										</button>
+									)}
+									onLogoutSuccess={logout}
+									cookiePolicy="single_host_origin"
+								/>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default UserProfile;
